4a3287cf57f2dc7797fff0138c844b36f5962ca3
NgnDinhThanh <thanh.nd200592@sis.hust.edu.vn>
2024-06-03 11:27:30 +0700

    Liệt kê xâu nhị phân không có 2 bit 1 đứng cạnh nhau

---
+++ backtracking/liet_ke_xau_nhi_phan_co_rang_buoc.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int x[100];
+int n;
+
+bool check(int v, int k)
+{
+    if (k == 1)
+        return true;
+    return x[k - 1] + v <= 1;
+}
+
+void solution()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << x[i] << " ";
+    }
+    cout << endl;
+}
+
+void Try(int k)
+{
+    for (int v = 0; v <= 1; v++)
+    {
+        if (check(v, k))
+        {
+            x[k] = v;
+            if (k == n)
+            {
+                solution();
+            }
+            else
+            {
+                Try(k + 1);
+            }
+        }
+    }
+}
+
+int main()
+{
+    n = 3;
+    Try(1);
+    return 0;
+}
